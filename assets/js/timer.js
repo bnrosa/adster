b9ad4a961ac54bfa5f793b631c76e095f0014615
bernardo-devsquad <bernardoorosa@gmail.com>
2020-10-09 09:04:24 -0300

    :bug: Fixed timer issue on admin pages

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,29 +1,29 @@
-//var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-
 // Update the count down every 1 second
-var x = setInterval(function() {
-    var countDown = document.getElementById('adsterCountdown').innerHTML * 1000;
-    console.log(countDown);
-    var now = (new Date().getTime());
-    console.log(now);
-    var distance = countDown - now;
-    console.log(distance);
-    if(distance > 0){
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        days = ('00'+days).slice(-2);
-        minutes = ('00'+minutes).slice(-2);
-        hours = ('00'+hours).slice(-2);
-        seconds = ('00'+seconds).slice(-2);
-    
-        document.getElementById("timerDays").innerHTML = days;
-        document.getElementById("timerHours").innerHTML = hours;
-        document.getElementById("timerMin").innerHTML = minutes;
-        document.getElementById("timerSec").innerHTML = seconds;
-    }
-    if (distance < 0) {
-        clearInterval(x);
-    }
-}, 1000);
+if(document.getElementById('adsterCountdown') != null){
+    var x = setInterval(function() {
+        var countDown = document.getElementById('adsterCountdown').innerHTML * 1000;
+        console.log(countDown);
+        var now = (new Date().getTime());
+        console.log(now);
+        var distance = countDown - now;
+        console.log(distance);
+        if(distance > 0){
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            days = ('00'+days).slice(-2);
+            minutes = ('00'+minutes).slice(-2);
+            hours = ('00'+hours).slice(-2);
+            seconds = ('00'+seconds).slice(-2);
+        
+            document.getElementById("timerDays").innerHTML = days;
+            document.getElementById("timerHours").innerHTML = hours;
+            document.getElementById("timerMin").innerHTML = minutes;
+            document.getElementById("timerSec").innerHTML = seconds;
+        }
+        if (distance < 0) {
+            clearInterval(x);
+        }
+    }, 1000);
+}
