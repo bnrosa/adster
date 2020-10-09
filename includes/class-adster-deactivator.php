c4c43cde578ca6786d07341c7777913e34380bb2
bernardo-devsquad <bernardoorosa@gmail.com>
2020-10-09 09:05:22 -0300

    :fire: Removed comments

--- includes/class-adster-deactivator.php
+++ includes/class-adster-deactivator.php
@@ -1,36 +1,7 @@
 <?php
 
-/**
- * Fired during plugin deactivation
- *
- * @link       https://github.com/bnrosa
- * @since      1.0.0
- *
- * @package    Adster
- * @subpackage Adster/includes
- */
-
-/**
- * Fired during plugin deactivation.
- *
- * This class defines all code necessary to run during the plugin's deactivation.
- *
- * @since      1.0.0
- * @package    Adster
- * @subpackage Adster/includes
- * @author     Bernardo Rosa <bernardoorosa@gmail.com>
- */
 class Adster_Deactivator {
-
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
 	public static function deactivate() {
 
 	}
-
 }
